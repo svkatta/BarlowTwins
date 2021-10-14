18e4d0b757fe442621f62093f13737024130e504
sandesh katta <sandeshkatta@outlook.com>
2021-10-14 14:26:00 +0530

    Minor fixes

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -1,4 +1,7 @@
 import torch
+import torchaudio
+# import librosa
+# import tensorflow as tf
 import numpy as np
 
 def collate_fn_padd_2b(cls,batch):
@@ -31,10 +34,11 @@ class DataUtils():
         ## padd
         
         batch_x = [torch.Tensor(t) for t,y in batch]
-        batch_y = [y for t,y in batch]
+        batch_y = [torch.Tensor(y) for t,y in batch]
         batch_x = torch.nn.utils.rnn.pad_sequence(batch_x,batch_first = True)
         batch_x = batch_x.unsqueeze(1)
-        batch_y = torch.Tensor(batch_y).type(torch.LongTensor)
+        batch_y = torch.nn.utils.rnn.pad_sequence(batch_y,batch_first = True)
+        batch_y = batch_y.unsqueeze(1)
 
         return batch_x,batch_y
 
