28cb33a95c604d9a0a1315209dd18a92823736c0
sandesh katta <sandeshkatta@outlook.com>
2021-10-14 14:51:41 +0530

    cleaned data utils

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -1,5 +1,5 @@
 import torch
-import torchaudio
+# import torchaudio
 # import librosa
 # import tensorflow as tf
 import numpy as np
@@ -21,25 +21,5 @@ def collate_fn_padd_2b(cls,batch):
     return batch_x,batch_y
 
 #-------------
-
-class DataUtils():
-
-    @classmethod
-    def collate_fn_padd_2(cls,batch):
-        '''
-        Padds batch of variable length
-        note: it converts things ToTensor manually here since the ToTensor transform
-        assume it takes in images rather than arbitrary tensors.
-        '''
-        ## padd
-        
-        batch_x = [torch.Tensor(t) for t,y in batch]
-        batch_y = [torch.Tensor(y) for t,y in batch]
-        batch_x = torch.nn.utils.rnn.pad_sequence(batch_x,batch_first = True)
-        batch_x = batch_x.unsqueeze(1)
-        batch_y = torch.nn.utils.rnn.pad_sequence(batch_y,batch_first = True)
-        batch_y = batch_y.unsqueeze(1)
-
-        return batch_x,batch_y
 
 #-------------Transforms Audio and Spectrogram-----------------
