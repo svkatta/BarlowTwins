f0b9d67705143c4ee300a0d225e67b121fa484fc
sandesh <sandeshkatta@outlook.com>
2021-10-20 12:29:12 +0530

    Wandbsupport (#3)
    * Minor fixes
    
    * cleaned data utils
    
    * added environment files
    
    * wandb support

--- main_upstream.py
+++ main_upstream.py
@@ -6,7 +6,8 @@ import signal
 import subprocess
 import sys
 import time
-
+import wandb
+wandb.login()
 
 from torch import nn
 import torch
@@ -17,22 +18,6 @@ from optmizers.lars import LARS , adjust_learning_rate
 
 from datasets.data_utils import collate_fn_padd_2b
 from utils import get_upstream_parser ,AverageMeter
-
-
-def create_dir(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-def get_logger(args):
-    (args.exp_dir / 'checkpoints').mkdir(parents=True, exist_ok=True)
-    logger = logging.getLogger("default")
-    f_handler = logging.FileHandler(os.path.join(args.exp_dir,'train.log'))
-    f_handler.setLevel(logging.INFO)
-    # f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # f_handler.setFormatter(f_format)
-    logger.addHandler(f_handler)
-    logger.setLevel(logging.DEBUG)
-    return logger
 
 
 def handle_sigusr1(signum, frame):
@@ -46,8 +31,9 @@ def handle_sigterm(signum, frame):
 
 
 def main_worker(gpu, args):
-    logger = get_logger(args)
     args.rank += gpu
+    if args.rank==0:
+        run = wandb.init(project="pytorch-demo", config=vars(args))
     torch.distributed.init_process_group(
         backend='nccl', init_method=args.dist_url,
         world_size=args.world_size, rank=args.rank)
@@ -80,7 +66,7 @@ def main_worker(gpu, args):
     
     if args.resume:
         ckpt = torch.load(args.checkpoint_file,map_location='cpu')
-        logger.info("Resuming pretrain from epoch {0}".format(ckpt['epoch']))
+        print("Resuming pretrain from epoch {0}".format(ckpt['epoch']))
         start_epoch = ckpt['epoch']
         model.load_state_dict(ckpt['model'])
         optimizer.load_state_dict(ckpt['optimizer'])
@@ -98,24 +84,22 @@ def main_worker(gpu, args):
 
     scaler = torch.cuda.amp.GradScaler()
     if args.rank == 0:
-            logger.info("Starting To Train")
+        print("Starting To Train")
+        wandb.watch(model, log="all", log_freq=10)
     for epoch in range(start_epoch, args.epochs):
         sampler.set_epoch(epoch)
-        loss =train_one_epoch(epoch,model,optimizer,loader,scaler,args,gpu,stats_file)
+        train_one_epoch(epoch,model,optimizer,loader,scaler,args,gpu,stats_file)
         
-        if args.rank == 0:
-            # logger.info('Epoch [{0}] ConvNet loss: {1:.3f}'.format(epoch, loss))
-
+        # if args.rank == 0:
             # save checkpoint            
-            torch.save({'epoch': epoch + 1,
-                        'model': model.state_dict(),
-                        'optimizer' : optimizer.state_dict()},
-                    args.exp_dir / 'checkpoints' / ('checkpoint_' + str(epoch + 1)  + '.pth'))
-
-    
+            # torch.save({'epoch': epoch + 1,
+            #             'model': model.state_dict(),
+            #             'optimizer' : optimizer.state_dict()},
+            #         args.exp_dir / 'checkpoints' / ('checkpoint_' + str(epoch + 1)  + '.pth'))
+    if args.rank==0:
+        run.finish()
 
 def train_one_epoch(epoch,model,optimizer,loader,scaler,args,gpu,stats_file):
-    logger = get_logger(args)
     # per epoch stats
     batch_time = AverageMeter()
     losses = AverageMeter()
@@ -144,6 +128,7 @@ def train_one_epoch(epoch,model,optimizer,loader,scaler,args,gpu,stats_file):
         end = time.time()
 
         if args.rank == 0:
+            wandb.log({"epoch": epoch, "instant_loss": loss}, step=step)
             print('Epoch: [{0}][{1}/{2}]\t'
                     'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                     'Data: {data_time.val:.3f} ({data_time.avg:.3f})\t'
@@ -152,14 +137,12 @@ def train_one_epoch(epoch,model,optimizer,loader,scaler,args,gpu,stats_file):
 
         if step % args.print_freq == 0:
             if args.rank == 0:
-                stats = dict(epoch=epoch, step=step,
-                                lr_weights=optimizer.param_groups[0]['lr'],
-                                lr_biases=optimizer.param_groups[1]['lr'],
-                                loss=loss.item())
-                print(json.dumps(stats))
-                print(json.dumps(stats), file=stats_file)
+                wandb.log({"lr_weights" :optimizer.param_groups[0]['lr'] ,
+                            "lr_biases" :optimizer.param_groups[1]['lr'],
+                            }, step=step)
     if args.rank == 0:
-        logger.info('Epoch [{0}] ConvNet loss: {1:.3f} , On Diag loss: {2:.3f} , Off Diag loss: {3:.3f} '.format(epoch, losses.avg,on_diag_losses.avg,off_diag_losses.avg))
+        wandb.log({"loss_epoch":losses.avg , "on_diag_loss": on_diag_losses.avg,
+                    "off_diag_loss" : off_diag_losses.avg },step=(epoch+1)*len(loader))
     return losses.avg
 
 
